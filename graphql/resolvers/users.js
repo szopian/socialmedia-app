e912c535ef8ffd58eeda314db72023ca5afb37bb
Adri Ana <szopiangreg@gmail.com>
2021-03-16 22:30:52 +0000

    add mutation register

---
+++ graphql/resolvers/users.js
@@ -0,0 +1,7 @@
+const User = require("../../models/User");
+
+module.export = {
+  Mutation: {
+    register(_, args, context, info)
+  }
+}
--- graphql/typeDefs.js
+++ graphql/typeDefs.js
@@ -7,7 +7,23 @@ module.exports = gql`
     createdAt: String!
     username: String!
   }
+  type{
+    id: ID!
+    email: String!
+    token: String!
+    username: String!
+    createdAt: String!
+  }
+  input RegisterImput{
+    username: String!
+    password: String!
+    confirmPassword: String!
+    email: String!
+  }
   type Query {
     getPosts: [Post]
+  }
+  type Mutation{
+    register(registerInput: RegisterImput): User!
   }
 `;
