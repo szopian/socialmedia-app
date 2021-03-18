2dbdfd30fe2200757a22b16b7676c55ba2eb5223
Adri Ana <szopiangreg@gmail.com>
2021-03-18 23:06:07 +0000

    commit likes

--- graphql/resolvers/posts.js
+++ graphql/resolvers/posts.js
@@ -64,5 +64,23 @@ module.exports = {
         throw new Error(err);
       }
     },
+    async likePost(_, { postId }, context) {
+      const { username } = checkAuth(context);
+
+      const post = await Post.findById(postId);
+      if (post) {
+        if (post.likes.find((like) => like.username === username)) {
+          post.likes = post.likes.filter((like) => like.username !== unsername);
+        } else {
+          post.likes.push({
+            username,
+            createdAt: new Date().toISOString(),
+          });
+        }
+
+        await post.save();
+        return post;
+      } else throw new UserInputError("Post not found");
+    },
   },
 };
