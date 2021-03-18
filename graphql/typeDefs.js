2af0b89495c9ac749cde93199b995e69b2b31508
Adri Ana <szopiangreg@gmail.com>
2021-03-18 22:28:19 +0000

    commit comments.js

--- graphql/typeDefs.js
+++ graphql/typeDefs.js
@@ -44,5 +44,6 @@ module.exports = gql`
     deletePost(postId: ID!): String!
     createComment(postId: String!, body: String!): Post!
     deleteComment(postId: ID!, commentId: ID!): Post!
+    likePost(postId: ID!): Post!
   }
 `;
