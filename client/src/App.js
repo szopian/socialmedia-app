a0ba7964f6bc6e5339c0e45ca9896fe03901c97a
Adri Ana <szopiangreg@gmail.com>
2021-03-23 11:28:53 +0000

    add login home register pages

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,10 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 import "semantic-ui-css/semantic.min.css";
 import "./App.css";
 
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+
 function App() {
   return (
     <Router>
--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-export default function Register() {
+function Register() {
   return (
     <div>
       <h1>Register Page</h1>
