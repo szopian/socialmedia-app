2ad5b6d23ead796554bfe75f5e5960de56ab7d8a
Adri Ana <szopiangreg@gmail.com>
2021-03-23 12:40:04 +0000

    fetch post query

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,11 +1,40 @@
 import React from "react";
+import { useQuery } from "@apollo/client";
+import gql from "graphql-tag";
 
 function Home() {
+  const { loading, data } = useQuery(FETCH_POSTS_QUERY);
+
+  if (data) {
+    console.log(data);
+  }
   return (
     <div>
       <h1>Home Page</h1>
     </div>
   );
 }
+
+const FETCH_POSTS_QUERY = gql`
+  {
+    getPosts {
+      id
+      body
+      createdAt
+      username
+      likeCount
+      likes {
+        username
+      }
+      commentCount
+      comments {
+        id
+        username
+        createdAt
+        body
+      }
+    }
+  }
+`;
 
 export default Home;
